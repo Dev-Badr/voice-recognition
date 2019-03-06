30b3c641edffb3f9814de35b3dd92b636b23a9fc
Badr Kamel <badrzorro@gmail.com>
2019-03-06 18:31:01 +0200

    first commet

---
+++ Voice Recognition/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace Voice_Recognition
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonX2 = new DevComponents.DotNetBar.ButtonX();
+            this.buttonX1 = new DevComponents.DotNetBar.ButtonX();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // buttonX2
+            // 
+            this.buttonX2.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.buttonX2.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.buttonX2.Location = new System.Drawing.Point(201, 265);
+            this.buttonX2.Name = "buttonX2";
+            this.buttonX2.Size = new System.Drawing.Size(158, 35);
+            this.buttonX2.Style = DevComponents.DotNetBar.eDotNetBarStyle.OfficeMobile2014;
+            this.buttonX2.TabIndex = 0;
+            this.buttonX2.Text = "Disable Voice Control";
+            this.buttonX2.Click += new System.EventHandler(this.buttonX2_Click);
+            // 
+            // buttonX1
+            // 
+            this.buttonX1.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.buttonX1.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.buttonX1.Location = new System.Drawing.Point(12, 265);
+            this.buttonX1.Name = "buttonX1";
+            this.buttonX1.Size = new System.Drawing.Size(158, 35);
+            this.buttonX1.Style = DevComponents.DotNetBar.eDotNetBarStyle.OfficeMobile2014;
+            this.buttonX1.TabIndex = 1;
+            this.buttonX1.Text = "Enable Voice Control";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 13);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(347, 236);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(371, 312);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.buttonX1);
+            this.Controls.Add(this.buttonX2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Voice Recognition";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevComponents.DotNetBar.ButtonX buttonX2;
+        private DevComponents.DotNetBar.ButtonX buttonX1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Voice Recognition/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+
+namespace Voice_Recognition
+{
+    public partial class Form1 : Form
+    {
+        SpeechRecognitionEngine recengine = new SpeechRecognitionEngine();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonX1_Click(object sender, EventArgs e)
+        {
+            recengine.RecognizeAsync(RecognizeMode.Multiple);
+            buttonX2.Enabled = true;
+        }
+
+        private void buttonX2_Click(object sender, EventArgs e)
+        {
+            buttonX2.Enabled = false;
+            recengine.RecognizeAsyncStop();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Choices commands = new Choices();
+            commands.Add(new string[] {"H","P", "C"});
+            GrammarBuilder gbuilder = new GrammarBuilder();
+            gbuilder.Append(commands);
+            Grammar grammar = new Grammar(gbuilder);
+            recengine.LoadGrammarAsync(grammar);
+            recengine.SetInputToDefaultAudioDevice();
+            recengine.SpeechRecognized += recengine_speech_recognized;
+        }
+
+        void  recengine_speech_recognized(object sender, SpeechRecognizedEventArgs e)
+
+        {
+            switch (e.Result.Text)
+            {
+                case "H":
+                        MessageBox.Show("Hello Sir. How are you?");
+                        break;
+                case "P":
+                    richTextBox1.Text += "\nBadr";
+                    break;
+                case "C":
+                    richTextBox1.Text += "\nCool";
+                    break;
+            }
+        }
+    }
+}
